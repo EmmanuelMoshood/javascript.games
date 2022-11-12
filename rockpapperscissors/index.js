5457b82c3715d0c69c1552374c307f3e97d88fd1
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2022-11-12 12:10:44 -0500

    logic completed

--- rockpapperscissors/index.js
+++ rockpapperscissors/index.js
@@ -16,17 +16,18 @@ let result
 choices.forEach(button => button.addEventListener("click", () => {
 
     playerChoice = button.textContent;
-    ComputerTurn(); 
     console.log(playerChoice);
+    ComputerTurn(); 
+    checkWinner();
     displayPlayerChoice.innerText = playerChoice;
-    displayResult.innerText = `You picked ${playerChoice} and computer picked ${computerChoice}. Hence ...`
+    displayResult.innerText = `You picked ${playerChoice} and computer picked ${computerChoice}. Hence ${checkWinner()}`
 
 }));
 
 function ComputerTurn(){
     const options = ['rock', 'scissors', 'paper'];
     const indexOFChoice = Math.floor(Math.random() * options.length)
-    const computerChoice = options[indexOFChoice]
+    computerChoice = options[indexOFChoice]
     console.log(computerChoice);
     displayComputerChoice.innerText = computerChoice; 
     return computerChoice  
@@ -36,3 +37,33 @@ function ComputerTurn(){
 //     let playerChoice = button.textContent;
 //     console.log(playerChoice);
 // }
+
+function checkWinner(){
+    if(playerChoice == computerChoice){
+        return "Draw"
+    }
+    else if(computerChoice == 'rock'){
+        return (playerChoice == 'paper') ? "You won" : "You lost"
+    }
+    else if(computerChoice == 'paper'){
+        return (playerChoice == 'scissors') ? "You won" : "You lost"
+    }
+    else if(computerChoice == 'scissors'){
+        return (playerChoice == 'rock') ? "You won" : "You lost"
+    }
+
+
+    // if(playerChoice === computerChoice){
+    //     return "it is a Tie"
+    // }
+    // else if(
+    //     playerChoice === 'rock' && computerChoice === 'scissors' ||
+    //     playerChoice === 'paper' && computerChoice === 'rock' ||
+    //     playerChoice === 'scissors' && computerChoice === 'paper' 
+    // ){
+    //     return "you won!"
+    // }
+    // else{
+    //     return "you lost"
+    // }
+}
